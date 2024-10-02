ec91cbcea4a895c0fa00944bb22686f1a4322e11
Iván Caro Romero <icarrom063@cpifpalanturing.es>
2024-10-02 14:01:32 +0200

    hello

--- src/app/personal-card/personal-card.component.ts
+++ src/app/personal-card/personal-card.component.ts
@@ -15,6 +15,13 @@ export class PersonalCardComponent  implements OnInit {
 
   constructor() { }
 
+  public people = [
+    { name: 'Iván', surname: 'Caro Romero', age: '19' },
+    { name: 'Adrian', surname: 'Barrios Márquez', age: '19' },
+    { name: 'Samuel', surname: 'Avella Pérez', age: '19' },
+    { name: 'Paco', surname: 'Flores Ramírez', age: '58' }
+  ];
+
   ngOnInit() {}
 
 }
